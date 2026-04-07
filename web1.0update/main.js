c415cbc03b9474824b0dae2d9612374cfbe763cc
soniceggman419-ship-it <soniceggman419@gmail.com>
2026-04-07 04:44:05 -0500

    Add link navigation and history management

--- web1.0update/main.js
+++ web1.0update/main.js
@@ -1,7 +1,22 @@
 import { fetchPage } from "./fetcher.js";
 import { sanitizeHTML } from "./sanitizer.js";
 import { renderPage } from "./renderer.js";
+import { enableLinkNavigation } from "./linkHandler.js";
+import { pushHistory } from "./history.js";
 
+
+async function loadPage() {
+  let url = input.value.trim();
+  if (!url.startsWith("http")) url = "https://" + url;
+
+  pushHistory(url);
+
+  const rawHTML = await fetchPage(url);
+  const cleanHTML = sanitizeHTML(rawHTML);
+  renderPage(cleanHTML);
+}
+const viewer = document.getElementById("viewer");
+enableLinkNavigation(viewer);
 const input = document.getElementById("urlInput");
 const button = document.getElementById("goBtn");
 
